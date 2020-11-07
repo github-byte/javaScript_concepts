fda10cf450feaac4e31eb43f62e581f3456bfaeb
Dipannita <dipannita223@gmail.com>
2020-11-07 19:48:23 +0530

    Currying

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import { add } from "lodash"
+import {compose,pipe} from "lodash/fp"
+
+
+
+
+// function sayHello(){
+//     return "Hello World";
+// }
+
+// let fn=sayHello;
+// //same as sayHello()
+
+// const input="JAVA"
+// const tab=type=>input=>`<${type}>${input}</${type}>`
+// const trim=input=>input.trim()
+
+// const toLowerCase=input=>input.toLowerCase()
+// compose(tab,toLowerCase,trim)
+// const transform =pipe(trim,toLowerCase,tab("div"))
+// console.log(transform(input))
+function sum(a){
+return function(b){
+return a+b
+}
+}
+
+console.log(sum(1)(2));
